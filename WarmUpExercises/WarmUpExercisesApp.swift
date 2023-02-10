81124a0092a77d00fb8eca1e564fa015a429dbbb
Abdurakhmon <abdur@basemap.com>
2023-02-10 15:02:24 +0500

    Initial Commit

---
+++ WarmUpExercises/WarmUpExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpExercisesApp.swift
+//  WarmUpExercises
+//
+//  Created by Workspace on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
